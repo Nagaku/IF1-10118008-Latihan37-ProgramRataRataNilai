01c6913b290cb415d3144ce60f2307ecb78b52e1
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:27 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan37-ProgramRataRataNilai/src/if1/pkg10118008/latihan37/programrataratanilai/IF110118008Latihan37ProgramRataRataNilai.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan37.programrataratanilai;
+
+/**
+ *
+* @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF-1
+ * NIM			: 10118008
+ * Deskripsi Program	: menghitung rata rata nilai mahasiswa
+ */
+public class IF110118008Latihan37ProgramRataRataNilai {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Nilai nilai = new Nilai();
+		nilai.inputNilai();
+		nilai.outputHasil();
+	}
+	
+}
